4d450a5bb7111d7d213176db3832241da7980688
ash99871 <asrobayop@eafit.edu.co>
2025-09-15 10:58:08 -0500

    Add files via upload

---
+++ Practice2.pl
@@ -0,0 +1,13 @@
+%Vehicles
+vehicle(toyota, yaris, sedan, 26000, 2020).
+vehicle(toyota, prado250, suv, 75200, 2023).
+vehicle(toyota, gr86, sport, 30000, 2025).
+vehicle(ford, bronco, suv, 29000, 2017).
+vehicle(toyota, tundra, pickup, 40000, 2025).
+vehicle(ford, maverick, pickup, 35000, 2012).
+vehicle(ford, f-150, sport, 30000, 2019).
+vehicle(ford, mustangGT, sedan, 30000, 2019).
+vehicle(bmw, coupex2, suv, 40000, 2025).
+vehicle(bmw, e30m3, pickup, 50000, 2022).
+vehicle(bmw, m2, sport, 29000, 2018).
+vehicle(ford, focusrs, sport, 32000, 2017).
